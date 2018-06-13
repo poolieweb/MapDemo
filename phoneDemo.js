186679ec9f21b8c9fe90f4f9b7da9d2641f052bc
Richard Poole <richard@Richards-MacPro.local>
2018-06-13 09:38:24 +0100

    Phone location

--- phoneDemo.js
+++ phoneDemo.js
@@ -1,3 +1,11 @@
+
+    // Main Map
+    var map = L.Wrld.map("map", "18ae0d05d0aafb494039a95baab0377f", {
+        center: [53.5339, -2.2552],
+        zoom: 16
+    });
+
+
 window.onload = function() {
     var startPos;
     var geoSuccess = function(position) {
