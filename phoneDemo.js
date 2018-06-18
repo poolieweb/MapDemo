8cc134d581f6137bc13b5d93124065ede7ab4bd2
Richard Poole <richard@Richards-MacPro.local>
2018-06-18 15:51:22 +0100

    adding local

--- phoneDemo.js
+++ phoneDemo.js
@@ -1,19 +1,17 @@
 
-    // Main Map
-    var map = L.Wrld.map("map", "18ae0d05d0aafb494039a95baab0377f", {
-        center: [53.5339, -2.2552],
-        zoom: 16
+var map = L.Wrld.map("map", "18ae0d05d0aafb494039a95baab0377f", {
+    center: [53.5339, -2.2552],
+    zoom: 16
+});
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition);
+} else { 
+    alert("Geolocation is not supported by this browser.");
+}
+    
+function showPosition(position) {
+    map.setView([position.coords.latitude, position.coords.longitude], 50, {
+        animate: false
     });
-
-
-window.onload = function() {
-    var startPos;
-    var geoSuccess = function(position) {
-      startPos = position;
-      document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-      document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-
-
-    };
-    navigator.geolocation.getCurrentPosition(geoSuccess);
-  };
+}
